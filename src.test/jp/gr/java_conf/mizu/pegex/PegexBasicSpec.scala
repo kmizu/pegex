1da86813f1706778efee318ea77a076d8d5929a6
Mizushima <mizukota@gmail.com>
2010-04-24 19:42:47 +0900

    Change to use Specs.

--- src.test/jp/gr/java_conf/mizu/pegex/PegexBasicSpec.scala
+++ src.test/jp/gr/java_conf/mizu/pegex/PegexBasicSpec.scala
@@ -1,65 +1,64 @@
 package jp.gr.java_conf.mizu.pegex
+import org.specs._
 
-object PegexBasicSpec {
-  def main(args: Array[String]) {
-    import jp.gr.java_conf.mizu.pegex.Pegex._
+object PegexBasicSpec extends Specification {
+  import jp.gr.java_conf.mizu.pegex.Pegex._
+  """PEGEX representing alphabet sequences""" in {
     val alphabets = """L=[a-zA-Z]+$;""".e
-    println("parsing alphabet ...")
-    println(alphabets.matches("HogeFooBar"))
-    println(alphabets.matches("Hoge_Foo_Bar"))
-    println("done.")
+    alphabets.matches("HogeFooBar") must_== Some("HogeFooBar")
+    alphabets.matches("Hoge_Foo_Bar") must_== None
+  }
+  """PEGEX representing identifiers""" in {
     val ident = """
       L=#(IdentStart)#(IdentRest)*$;
       IdentStart=[a-zA-Z_];
       IdentRest=#(IdentStart)|[0-9];
-    """.e
-    println("parsing identifier ...")
-    println(ident.matches("HogeFooBar"))
-    println(ident.matches("Hoge_Foo_Bar"))
-    println(ident.matches("Hoge10"))
-    println(ident.matches("10Hoge"))
-    println("done.")
+    """.e    
+    ident.matches("HogeFooBar") must_== Some("HogeFooBar")
+    ident.matches("Hoge_Foo_Bar") must_== Some("Hoge_Foo_Bar")
+    ident.matches("Hoge10") must_== Some("Hoge10")
+    ident.matches("10Hoge") must_== None
+  }
+  """PEGEX not matching anything""" in {
     val nothing = """L=a*a;""".e(likeRegex = false)
-    println("parsing nothing ...")
-    println(nothing.matches("aaa")) // It should be None
-    println("done.")
-    println("parsing a1OrMore ...")
+    nothing.matches("aaa") must_== None
+  }
+  """PEGEX(greedy mode) representing one or more repetition of "a"""" in {
     val a1OrMore = """L=a*a;""".e(likeRegex = true)
-    println(a1OrMore.matches("aaa")) // It should be Some(aaa)
-    println("done.")
-    println("parsing nested comment ...")
+    a1OrMore.matches("aaa") must_== Some("aaa")
+  }
+  """PEGEX representing nested comments""" in {
     val comment = """L=#(C)$; C=/\*(#(C)|!(\*/).)*\*/;""".e
-    println(comment.matches("/* comment */"))
-    println(comment.matches("/* nested /* comment */ ok */"))
-    println(comment.matches("/* incorrect comment"))
+    comment.matches("/* comment */") must_== Some("/* comment */")
+    comment.matches("/* nested /* comment */ ok */") must_== Some("/* nested /* comment */ ok */")
+    comment.matches("/* incorrect comment") must_== None
+  }
+  """PEGEX representing subset of XMLs""" in {
     println("done.")
     println("parsing minXml ...")
     val minXml = """
       L=#(E)$; E=<#(tag:I)>#(E)*</##(tag)>; I=[a-z]+;""".e
-    println(minXml.matches("<foo></foo>"))
-    println(minXml.matches("<foo><bar></bar></foo>"))
-    println(minXml.matches("<foo><bar></bar></hoo>"))
-    println("done.")
-    println("parsing csl ...")
+    minXml.matches("<foo></foo>") must_== Some("<foo></foo>")
+    minXml.matches("<foo><bar></bar></foo>") must_== Some("<foo><bar></bar></foo>")
+    minXml.matches("<foo><bar></bar></hoo>") must_== None
+  }
+  """PEGEX representing a context sensitive language""" in {
     val csl = """S=&(#(A)!b)a+#(B)$; A=a#(A)?b; B=b#(B)?c;""".e
-    println(csl.matches("aaabbbccc"))
-    println(csl.matches("aabbbccc"))
-    println("done.")
-    println("parsing palindrome ...")
-    val palindrome = new Pegex("""
-      L=#(A)$; A=a#(A)a|b#(A)b|c#(A)c|a|b|c|_;
-    """, likeRegex = true)
-    println(palindrome.matches("abcba"))
-    println(palindrome.matches("abba"))
-    println(palindrome.matches("abc"))
-    println("done.")
+    csl.matches("aaabbbccc") must_== Some("aaabbbccc")
+    csl.matches("aabbbccc") must_== None
+  }
+  """PEGEX representing palindromes""" in {
+    val palindrome = """L=#(A)$; A=a#(A)a|b#(A)b|c#(A)c|a|b|c|_;""".e(likeRegex=true)
+    palindrome.matches("abcba") must_== Some("abcba")
+    palindrome.matches("abba") must_== Some("abba")
+    palindrome.matches("abc") must_== Some("abc")
+  }
+  """PEGEX representing (ab)^n, where n is even number.  This is test of backreference feature""" in {
     println("parsing testBackref ...")
-    val testBackref = new Pegex("""
-      L=#(A::(ab)+)##(A)$;
-    """, likeRegex = true)
-    println(testBackref.matches("ab"))
-    println(testBackref.matches("abab"))
-    println(testBackref.matches("ababab"))
-    println(testBackref.matches("abababab"))
+    val testBackref = """L=#(A::(ab)+)##(A)$;""".e(likeRegex=true)
+    testBackref.matches("ab") must_== None
+    testBackref.matches("abab") must_== Some("abab")
+    testBackref.matches("ababab") must_== None
+    testBackref.matches("abababab") must_== Some("abababab")
   }
 }
