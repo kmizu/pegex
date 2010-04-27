f1f78537e09b957688b53a3cdf1683eaf1ce604a
Mizushima <mizukota@gmail.com>
2010-04-27 15:33:13 +0900

    Now MatchResult extends DefaultMap.  Then, users can use MathResult as if it is Map.

--- src/jp/gr/java_conf/mizu/pegex/MatchResult.scala
+++ src/jp/gr/java_conf/mizu/pegex/MatchResult.scala
@@ -1,3 +1,7 @@
 package jp.gr.java_conf.mizu.pegex
+import scala.collection.{DefaultMap, Map}
 
-case class MatchResult(result: Option[String], group: Map[Symbol, String])
+case class MatchResult(result: Option[String], group: Map[Symbol, String]) extends DefaultMap[Symbol, String] {
+  def iterator: Iterator[(Symbol, String)] = group.iterator
+  def get(key: Symbol): Option[String] = group.get(key)
+}
