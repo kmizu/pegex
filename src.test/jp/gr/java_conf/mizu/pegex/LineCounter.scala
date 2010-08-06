6ca125d94e9978e888026e2851ed81a273965312
kmizu <mizukota@gmail.com>
2010-08-06 16:38:43 +0900

    Rename method LineCounter.eachFile to LineCounter.foreachFile

--- src.test/jp/gr/java_conf/mizu/pegex/LineCounter.scala
+++ src.test/jp/gr/java_conf/mizu/pegex/LineCounter.scala
@@ -3,16 +3,16 @@ import java.io.File
 import scala.io.Source
 
 object LineCounter {
-  def eachFile(file: File, suffix: String)(block: File => Unit) {
+  def foreachFile(file: File, suffix: String)(block: File => Unit) {
     if(file.isDirectory) {
-      file.listFiles().foreach(eachFile(_, suffix)(block))
+      file.listFiles().foreach(foreachFile(_, suffix)(block))
     }else {
       if(file.getName.endsWith(suffix)) block(file)
     }
   }
   def main(args: Array[String]) {
     var count = 0
-    eachFile(new File("src"), ".scala"){file =>
+    foreachFile(new File("src"), ".scala"){file =>
       val source = Source.fromFile(file)
       count += source.getLines.toList.size
       source.close()
