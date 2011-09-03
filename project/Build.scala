1f540a394718ed162da4f0efda9f67476ed640e4
kmizu <mizukota@gmail.com>
2011-09-03 23:16:13 +0900

    Remove unneeded files.

--- project/Build.scala
+++ project/Build.scala
@@ -4,4 +4,8 @@ import Keys._
 object PegexBuild extends Build {
   // Declare a project in the root directory of the build with ID "pegex".
   lazy val root = Project("pegex", file("."), settings = Defaults.defaultSettings ++ Seq(name := "pegex", version := "0.1"))
+  lazy val specsRepo = "org.specs2" % "specs2" % "2_2.9.1"
+  libraryDependencies ++= Seq(
+      "org.specs2" %% "specs2" % "2_2.9.1"
+  )
 }
